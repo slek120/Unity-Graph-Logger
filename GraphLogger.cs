f17b875a4563aecd9fd8a2a1126e272556e95744
slek120 <slek120@gmail.com>
2014-04-01 20:02:46 +0700

    Add support for int and string

--- GraphLogger.cs
+++ GraphLogger.cs
@@ -2,17 +2,40 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 public class GraphLogger : MonoBehaviour
 {
-	// A List with min and max properties
+	// A List to track a property with min and max properties
 	class Line
 	{
-		public List<Vector2> points = new List<Vector2> ();
-		public float minPoint;
-		public float maxPoint;
-		public float ySize;
+		public SortedDictionary<int, string> strings;	// Strings with frame number for key
+		public SortedDictionary<int, float> points;		// Numbers with frame number for key
+		public bool isNumber;							// Tracked parameter is number or string
+		public float minPoint;							// Smallest number in points
+		public float maxPoint;							// Largest number in points
+		public float ySize;								// Scale factor for y-axis
+
+		public Line (int t, float x, float y)			// Constructor for number tracker
+		{
+			isNumber = true;
+			minPoint = x;
+			maxPoint = x;
+			ySize = y;
+			points = new SortedDictionary<int, float> ();
+			points.Add (t, x);
+		}
+
+		public Line (int t, string s)					// Constructor for string tracker
+		{
+			isNumber = false;
+			strings = new SortedDictionary<int, string> ();
+			strings.Add (t, s);
+		}
 	}
+	
+	// Static instance of the class
+	public static GraphLogger graphLogger;
 	
 	// Settings
 	public enum Format
@@ -21,12 +44,11 @@ public class GraphLogger : MonoBehaviour
 		Table
 	}
 	;
-	public static GraphLogger graphLogger;
-	public bool appendLogFile = false;
-	public Format format = Format.Table;
-	public int maxCount = 6000;
-	public bool graphOn = true;
-	public Color[] colors = new Color[6] {
+	public bool appendLogFile = false;				// Append or overwrite log file
+	public Format format = Format.Table;			// Format for saved log file
+	public int maxCount = 6000;						// Number of points logged per property
+	public bool graphOn = true;						// Display a graph on screen
+	public Color[] colors = new Color[6] {			// Colors of lines for graph
 		Color.blue,
 		Color.cyan,
 		Color.green,
@@ -34,22 +56,26 @@ public class GraphLogger : MonoBehaviour
 		Color.red,
 		Color.yellow
 	};
-	public Vector2 size = new Vector2 (300, 150);
-	public Vector2 offset = new Vector2 (10, 10);
-
-	// Material for lines (Default-Diffuse)
-	Material mat;
-	// Dictionary of properties being debugged
-	Dictionary<string, Line> lines = new Dictionary<string, Line> ();
-	// Conversion factor for screen to viewport space
-	float xScale;
+	public Vector2 size = new Vector2 (300, 150);	// Area of graph (px)
+	public Vector2 offset = new Vector2 (10, 10);	// Distance from bottom left corner (px)
+	
+	// Saved information
+	Material mat;									// Material for lines (Default-Diffuse)
+	Dictionary<string, Line> lines					// Dictionary of properties being debugged
+		= new Dictionary<string, Line> ();
+	float xScale;									// Conversion factor for screen to viewport space
 	float yScale;
-	// Scale factor for x-axis
-	float xSize;
-
+	float xSize;									// Scale factor for x-axis
+	
 	void Awake ()
 	{
-		graphLogger = this;
+		// Only have one instance of graphLogger
+		if (graphLogger == null) {
+			graphLogger = this;
+		} else {
+			Destroy (this);
+		}
+		
 		xScale = 1 / (float)Screen.width;
 		yScale = 1 / (float)Screen.height;
 		mat = new Material ("Shader \"Lines/Colored Blended\" {" +
@@ -63,31 +89,32 @@ public class GraphLogger : MonoBehaviour
 		mat.shader.hideFlags = HideFlags.HideAndDontSave;
 	}
 
+	public void AddPoint (int point)
+	{
+		AddPoint ((float)point, "default");
+	}
+
+	public void AddPoint (int point, string name)
+	{
+		AddPoint ((float)point, name);
+	}
+
 	public void AddPoint (float point)
 	{
 		AddPoint (point, "default");
 	}
-
+	
 	public void AddPoint (float point, string name)
 	{
-		float time = Time.frameCount;
-
-		if (!lines.ContainsKey (name))
-			lines.Add (name, new Line ());
-
-		Line line = lines [name];
-
-		if (line.points.Count == 0) {
-			line.minPoint = point;
-			line.maxPoint = point;
-			xSize = size.x;
-			line.ySize = size.y;
-		}
-
-		line.points.Add (new Vector2 (time, point));
-		
+		int time = Time.frameCount;
 		xSize = size.x / time;
-
+		
+		if (!lines.ContainsKey (name))
+			lines.Add (name, new Line (time, point, size.y));
+		else
+			lines [name].points.Add (time, point);
+		
+		Line line = lines [name];	
 		if (point < line.minPoint) {
 			line.minPoint = point;
 			if (line.minPoint != line.maxPoint)
@@ -97,12 +124,29 @@ public class GraphLogger : MonoBehaviour
 			if (line.minPoint != line.maxPoint)
 				line.ySize = size.y / (line.maxPoint - line.minPoint);
 		}
-
-		if (line.points.Count > maxCount) {
-			line.points.RemoveAt (0);
-		}
+		
+		if (line.points.Count > maxCount)
+			line.points.Remove (line.points.Keys.First ());
 	}
 
+	public void AddPoint (string point)
+	{
+		AddPoint (point, "default");
+	}
+	
+	public void AddPoint (string point, string name)
+	{
+		int time = Time.frameCount;
+		
+		if (!lines.ContainsKey (name))
+			lines.Add (name, new Line (time, point));
+		else
+			lines [name].strings.Add (time, point);
+
+		if (lines [name].strings.Count > maxCount)
+			lines [name].strings.Remove (lines [name].strings.Keys.First ());
+	}
+	
 	// Save then quit
 	void OnApplicationQuit ()
 	{
@@ -111,19 +155,19 @@ public class GraphLogger : MonoBehaviour
 		SaveLog ();
 		Application.Quit ();
 	}
-
+	
 	void OnApplicationPause (bool paused)
 	{
 		if (paused)
 			SaveLog ();
 	}
-
+	
 	void OnApplicationFocus (bool paused)
 	{
 		if (paused)
 			SaveLog ();
 	}
-
+	
 	public void SaveLog ()
 	{
 		try {
@@ -144,8 +188,14 @@ public class GraphLogger : MonoBehaviour
 		foreach (KeyValuePair<string,Line> element in lines) {
 			file.WriteLine ("Frame," + element.Key);
 			Line line = element.Value;
-			foreach (Vector2 point in line.points) {
-				file.WriteLine (point.x + "," + point.y);
+			if (line.isNumber) {
+				foreach (KeyValuePair<int, float> kvp in line.points) {
+					file.WriteLine (kvp.Key + "," + kvp.Value);
+				}
+			} else {
+				foreach (KeyValuePair<int, string> kvp in line.strings) {
+					file.WriteLine (kvp.Key + "," + kvp.Value);
+				}
 			}
 			file.WriteLine ();
 		}
@@ -154,11 +204,19 @@ public class GraphLogger : MonoBehaviour
 	void writeTable (StreamWriter file)
 	{
 		// Get all framecounts
-		SortedDictionary<int,string> rows = new SortedDictionary<int, string> ();
-		foreach (KeyValuePair<string,Line> element in lines) {
-			foreach (Vector2 point in element.Value.points) {
-				if (!rows.ContainsKey ((int)point.x))
-					rows.Add ((int)point.x, "");
+		SortedDictionary<int, string> rows = new SortedDictionary<int, string> ();
+		foreach (KeyValuePair<string, Line> element in lines) {
+			Line line = element.Value;
+			if (line.isNumber) {
+				foreach (int key in line.points.Keys) {
+					if (!rows.ContainsKey (key))
+						rows.Add (key, "");
+				}
+			} else {
+				foreach (int key in line.strings.Keys) {
+					if (!rows.ContainsKey (key))
+						rows.Add (key, "");
+				}
 			}
 		}
 		
@@ -168,12 +226,20 @@ public class GraphLogger : MonoBehaviour
 			foreach (int key in keys) {
 				rows [key] += ",";
 			}
-			foreach (Vector2 point in element.Value.points) {
-				if (rows.ContainsKey ((int)point.x))
-					rows [(int)point.x] += point.y.ToString ();
+			Line line = element.Value;
+			if (line.isNumber) {
+				foreach (KeyValuePair<int, float> kvp in line.points) {
+					if (rows.ContainsKey (kvp.Key))
+						rows [kvp.Key] += kvp.Value.ToString ();
+				}
+			} else {
+				foreach (KeyValuePair<int, string> kvp in line.strings) {
+					if (rows.ContainsKey (kvp.Key))
+						rows [kvp.Key] += kvp.Value.ToString ();
+				}
 			}
 		}
-
+		
 		// Write first row
 		file.Write ("Frame");
 		foreach (KeyValuePair<string,Line> element in lines) {
@@ -186,28 +252,31 @@ public class GraphLogger : MonoBehaviour
 		}
 		file.WriteLine ();
 	}
-
+	
 	// Draw graph
 	void OnPostRender ()
 	{
 		if (!graphOn)
 			return;
-
+		
 		GL.PushMatrix ();
 		mat.SetPass (0);
 		GL.LoadOrtho ();
 		GL.Begin (GL.LINES);
+
 		int i = 0;
 		foreach (KeyValuePair<string,Line> element in lines) {
 			Line line = element.Value;
-			float prevX = (line.points [0].x * xSize + offset.x) * xScale;
-			float prevY = ((line.points [0].y - line.minPoint) * line.ySize + offset.y) * yScale;
+			if (!line.isNumber)
+				break;
+			float prevX = (line.points.Keys.First () * xSize + offset.x) * xScale;
+			float prevY = ((line.points [line.points.Keys.First ()] - line.minPoint) * line.ySize + offset.y) * yScale;
 			GL.Color (colors [i % 6]);
-			foreach (Vector2 point in line.points) {
+			foreach (KeyValuePair<int, float> kvp in line.points) {
 				// Stretch points so that min is at offset and max is at offset+size
 				// Then convert to viewport space
-				float x = (point.x * xSize + offset.x) * xScale;
-				float y = ((point.y - line.minPoint) * line.ySize + offset.y) * yScale;
+				float x = (kvp.Key * xSize + offset.x) * xScale;
+				float y = ((kvp.Value - line.minPoint) * line.ySize + offset.y) * yScale;
 				GL.Vertex3 (prevX, prevY, 0);
 				GL.Vertex3 (x, y, 0);
 				prevX = x;
@@ -215,6 +284,7 @@ public class GraphLogger : MonoBehaviour
 			}
 			i++;
 		}
+
 		GL.End ();
 		GL.PopMatrix ();
 	}
